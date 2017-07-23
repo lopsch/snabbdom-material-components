2193ad1b412629ea0c294f4f39971bd945850f52
Daniel Lopes <lopsch@googlemail.com>
2017-07-23 12:16:40 +0200

    closes #2, implement onChange handling for checkboxes

--- src/components/checkbox/Checkbox.js
+++ src/components/checkbox/Checkbox.js
@@ -17,10 +17,11 @@ export default class Checkbox extends MaterialComponent {
   constructor (_props, _children) {
     super(_props, _children, STYLE_SWITCHES)
 
-    const { name, value, ...otherProps } = this.props
+    const { name, value, onChange, ...otherProps } = this.props
     this.name = this.utils.makeKeyValue('name', name)
     this.value = this.utils.makeKeyValue('value', value)
     this.hooks = this.utils.makeHooks(CheckboxAdapter)
+    this.onChange = typeof onChange === 'function' ? { change: onChange } : {}
     this.props = otherProps
   }
 
@@ -34,6 +35,7 @@ export default class Checkbox extends MaterialComponent {
         <input
           {...this.selector}
           classNames={CB_NC_CLASS}
+          on={this.onChange}
           type='checkbox'
           {...this.value}
           {...this.name}
