d62557587e70337598c47a70ea93bd3d11e7d5c4
Daniel Lopes <lopsch@googlemail.com>
2017-08-21 15:50:04 +0200

    prepare semantic

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -4,20 +4,20 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import progress from 'rollup-plugin-progress'
 import eslint from 'rollup-plugin-eslint'
+import minify from 'rollup-plugin-babel-minify'
 import rolluprc from './.rolluprc.json'
 
 export default [
   {
-    entry: rolluprc.entryConfig,
-    dest: pkg.main,
-    format: 'cjs',
+    input: rolluprc.entryConfig,
+    output: [{ file: pkg.main, format: 'cjs', sourcemap: false }],
     plugins: [
       progress({ clearLine: false }),
       eslint({ throwOnError: true, throwOnWarning: true }),
       resolve(),
       commonjs(),
-      babel(rolluprc.babelConfig)
-    ],
-    sourceMap: false
+      babel(rolluprc.babelConfig),
+      minify()
+    ]
   }
 ]
--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -1,24 +1,45 @@
 import pkg from './package.json'
 import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
 import progress from 'rollup-plugin-progress'
+import eslint from 'rollup-plugin-eslint'
 import minify from 'rollup-plugin-babel-minify'
 import rolluprc from './.rolluprc.json'
 
 export default [
   {
-    entry: rolluprc.entryConfig,
-    targets: [
-      { dest: pkg.module, format: 'es' },
-      { dest: pkg.main, format: 'cjs' }
+    input: rolluprc.entryConfig,
+    output: [
+      {
+        file: pkg.browser,
+        format: 'umd',
+        name: 'snabbdom-material-components',
+        sourcemap: true
+      }
     ],
     plugins: [
       progress({ clearLine: false }),
+      eslint({ throwOnError: true, throwOnWarning: true }),
+      resolve(),
+      commonjs(),
+      babel(rolluprc.babelConfig),
+      minify()
+    ]
+  },
+  {
+    input: rolluprc.entryConfig,
+    output: [
+      { file: pkg.module, format: 'es', sourcemap: true },
+      { file: pkg.main, format: 'cjs', sourcemap: true }
+    ],
+    plugins: [
+      progress({ clearLine: false }),
+      eslint({ throwOnError: true, throwOnWarning: true }),
       resolve(),
       babel(rolluprc.babelConfig),
       minify()
     ],
-    sourceMap: true,
     external: id => rolluprc.externalConfig.includes(id)
   }
 ]
